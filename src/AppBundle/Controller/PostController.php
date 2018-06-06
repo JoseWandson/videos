e2b9ade748d9e1acb071275f8d13ee104ebf013b
Wa <wands@Wandson>
2018-06-05 22:07:49 -0300

    Criação da classe PostType.

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -21,6 +22,17 @@ class PostController extends Controller
         $posts = $this->getDoctrine()->getRepository("AppBundle:Post")->findAll();
 
         return $this->render("posts/posts.html.twig", ['posts' => $posts]);
+    }
+
+    /**
+     * @Route("/create")
+     * @return Response
+     */
+    public function createAction()
+    {
+        $form = $this->createForm(PostType::class);
+
+        return $this->render("posts/create.html.twig", ['form' => $form->createView()]);
     }
 
     /**
